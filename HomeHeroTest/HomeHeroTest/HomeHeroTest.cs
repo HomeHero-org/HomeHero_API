1338b7180d20f5bfc3999f24c99ea814ebe7e0c1
jdmooreno211 <jdmorenobeltran@gmail.com>
2023-09-20 20:49:07 -0500

    Add more tests

--- HomeHeroTest/HomeHeroTest/HomeHeroTest.cs
+++ HomeHeroTest/HomeHeroTest/HomeHeroTest.cs
@@ -5,6 +5,14 @@ using System.Threading.Tasks;
 using System.Linq;
 using System.Linq.Expressions;
 using HomeHero_API.Repository.IRepository;
+using HomeHero_API.Data;
+using HomeHero_API.Repository;
+using Microsoft.EntityFrameworkCore;
+using HomeHero_API.Models;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.DataCollection;
+using System.Threading;
+using System.Threading.Tasks;
+
 
 namespace HomeHeroTest
 {
@@ -39,7 +47,6 @@ namespace HomeHeroTest
         {
             var person = new Person { Id = 1 };
             _repoMock.Setup(repo => repo.Get(It.IsAny<Expression<Func<Person, bool>>>(), false)).ReturnsAsync(person);
-
             var result = await _repoMock.Object.Get(x => x.Id == 1, false);
             Assert.NotNull(result);
         }
@@ -64,5 +71,58 @@ namespace HomeHeroTest
     {
         public int Id { get; set; }
         // Otros campos aquí...
+    }
+    public class RequestRepositoryTests
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly RequestRepository _repository;
+
+        public RequestRepositoryTests()
+        {
+            // Configuración de la base de datos en memoria
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()) // Usar un nombre único para cada test
+                .Options;
+
+            _context = new ApplicationDbContext(options);
+            _repository = new RequestRepository(_context);
+        }
+        [Fact]
+        public void Update_ShouldUpdateRequestAndSetUpdateTime()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            using var context = new ApplicationDbContext(options);
+            var repository = new RequestRepository(context);
+
+            var request = new Request
+            {
+                RequestID = 1,
+                RequestTitle = "Original Title",
+                RequestContent = "Some content here", // <- Añade contenido aquí
+                RequestPicture = new byte[] { },      // <- Añade alguna imagen en formato de byte array aquí si es necesario
+                CreatedTime = DateTime.Now,
+                UpdateTime = DateTime.Now
+            };
+
+            // Supongamos que añades la solicitud original al contexto
+            context.Request.Add(request);
+            context.SaveChanges();
+
+            // Ahora, actualiza el título de la solicitud
+            request.RequestTitle = "Updated Title";
+
+            // Aquí podrías llamar al método de tu repositorio que realiza la actualización
+            repository.Update(request);
+            context.SaveChanges();
+
+            // Ahora puedes obtener la solicitud nuevamente del contexto y verificar que los cambios se hayan realizado
+            var updatedRequest = context.Request.Find(1);
+
+            Assert.Equal("Updated Title", updatedRequest.RequestTitle);
+            // Aquí también podrías verificar otros cambios que hayas hecho, como verificar que `UpdateTime` ha sido modificado
+        }
     }
 }
